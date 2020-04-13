4ddf42009b8564a993942029d7d639606b8e61db
mahadi-nsu <mahadi.hassan@northsouth.edu>
2020-04-13 21:42:32 +0600

    actions reducers and providers are created

---
+++ src/reducers/index.js
@@ -0,0 +1,21 @@
+import { combineReducers } from 'redux';
+const songsReducer = () => {
+    return [
+        { title: 'No scrubs', duration: '3:05' },
+        { title: 'Macarena', duration: '4:00' },
+        { title: 'Numb', duration: '4:05' },
+        { title: 'In the end', duration: '4:45' },
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+});
